acd61aa295b8ea68ef00749a1db984f4f902c21f
rpignatiello <rpignatiello31@gmail.com>
2023-11-28 12:25:55 -0500

    MSE loss function implementation

---
+++ models/loss.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+
+class JointsMSELoss(nn.Module):
+  def __init__(self):
+    super(JointsMSELoss, self).__init__()
+    self.mse = nn.MSELoss(size_average=True)
+    
+  def forward(self, output, target):
+    batch_size = output.shape[0]
+    num_joints = output.shape[0] # get number of joints for this image
+    heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1) # reshape heatmap predictions
+    heatmaps_target = target.reshape((batch_size, num_joints, -1)).split(1, 1) # reshape target values 
+    loss = 0
+
+    for joint_idx in range(num_joints):
+      heatmap_pred = heatmaps_pred[joint_idx].squeeze()
+      heatmap_target = heatmaps_target[joint_idx].squeeze()
+
+      loss += 0.5 * self.criterion(heatmap_pred, heatmap_target)
+    
+    # average loss per joint because number of joints in images are variable
+    return loss / num_joints 
