ef510e0c7754202bfb637176ebd4af467acc7da7
MarioXerri <xmario02@yahoo.com>
2023-12-07 19:10:20 -0500

    Edited Datasloader to Support Shuffling

--- datasets/custom_datasets.py
+++ datasets/custom_datasets.py
@@ -42,10 +42,12 @@ class LSPDataset():
 
   Parameters:
     - image_labels: list of joint annotations of shape (n,14,3)
+    - image_name: list of image names corresponding to the joint annotations at the same index 
     - img_dir: image directory to the location storing images
   """
-  def __init__(self, image_labels, img_dir):
+  def __init__(self, image_labels, image_names, img_dir):
     self.image_labels = image_labels
+    self.image_names = image_names
     self.img_dir = img_dir
     # can add transforms if we need/want
 
@@ -53,14 +55,13 @@ class LSPDataset():
     return (self.image_labels.shape[0])
 
   def __getitem__(self, idx):
-    # inputs
     # idx: int -> index of image sample
-    #
     # image: torch.Tensor -> pytorch tensor representing image specified
     # label: list[dict] -> each dict contains x and y coordinate, joint id, and whether the joint is visible
 
-    # img_path = self.img_dir + self.image_labels[idx]['image']['name']
-    img_path = self.img_dir + "resized_im" + '0'*(5-len(str(idx+1))) + str(idx + 1) + ".jpg"
+    
+    # img_path = self.img_dir + "resized_im" + '0'*(5-len(str(idx+1))) + str(idx + 1) + ".jpg"
+    img_path = self.img_dir + self.image_names[idx]
     image = read_image(img_path)
 
     # generate heatmap label 
