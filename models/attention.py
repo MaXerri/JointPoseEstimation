fixed the softmax problem

models/attention.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from models.constants_embeddings import NUM_CHANNELS, HIDDEN_SIZE, HIDDEN_DROPOUT_PROB, BATCH_SIZE, ATTENTION_HEADS, PATCH_DIM
+import math
 
 class attentionHead(nn.Module):
   def __init__(self):
@@ -18,7 +19,8 @@ class attentionHead(nn.Module):
     x = torch.reshape(x,(batch_size,ATTENTION_HEADS,3,PATCH_DIM*PATCH_DIM,HIDDEN_SIZE//ATTENTION_HEADS)) 
     Q,K,V = torch.unbind(x,2)
     QK = torch.matmul(Q,torch.transpose(K,3,2))
-    # QK = F.softmax(QK,dim = -1) #May need to swap dimension of softmax
+    QK = QK / math.sqrt(HIDDEN_SIZE//ATTENTION_HEADS)
+    QK = F.softmax(QK,dim = -1) #May need to swap dimension of softmax
     out = torch.matmul(QK,V)
     out = torch.transpose(out,1,2)
     out = torch.flatten(out, start_dim = 2)