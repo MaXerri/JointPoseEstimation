e1080faecb06639c4cd3910100335f60fd1c380b
rpignatiello <rpignatiello31@gmail.com>
2023-12-13 16:41:14 -0500

    more url changes

--- ui/constants.js
+++ ui/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://fz1ua1qe43.execute-api.us-east-1.amazonaws.com/prod/";
+export const API_URL = "https://ku2ggpdrzd.execute-api.us-east-1.amazonaws.com/prod/";
 export const BUCKET_NAME = "cs4701jointposeestimation";
 export const REGION = "us-east-1";
 export const IDENTITY_POOL_ID = "us-east-1:8b0a06a0-1abe-4009-b03c-55311ceb78aa";
