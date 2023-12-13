05496cc70add37aad177c740abbf33c98715f725
rpignatiello <rpignatiello31@gmail.com>
2023-12-13 15:59:39 -0500

    updating api endpoint

--- ui/constants.js
+++ ui/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://ku2ggpdrzd.execute-api.us-east-1.amazonaws.com/prod/";
+export const API_URL = "arn:aws:execute-api:us-east-1:472763113568:ku2ggpdrzd/*/POST/";
 export const BUCKET_NAME = "cs4701jointposeestimation";
 export const REGION = "us-east-1";
 export const IDENTITY_POOL_ID = "us-east-1:8b0a06a0-1abe-4009-b03c-55311ceb78aa";
