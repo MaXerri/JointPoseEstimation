3301643dbd4dd505417f4b501af17613c0dc6bcf
MarioXerri <xmario02@yahoo.com>
2024-08-06 09:14:26 -0400

    Made activation a param of the MLP
    - allows exchangability of activation function

--- models/feedforward.py
+++ models/feedforward.py
@@ -7,13 +7,16 @@ class feedForward(nn.Module):
   """
   MLP for the transformer block
   """
-  def __init__(self, activ):
+  def __init__(self, activ, mlp_interm_factor = 4):
     super().__init__()
     self.indim = HIDDEN_SIZE
+    self.mlp_intermediate_dim = mlp_interm_factor * HIDDEN_SIZE
     self.outdim = HIDDEN_SIZE
-    self.lay1 = nn.Linear(self.indim,self.outdim)
-    self.lay2 = nn.Linear(self.outdim,self.indim)
-    self.activ = activ() # test out changing to differnt activations
+
+    self.lay1 = nn.Linear(self.indim,self.mlp_intermediate_dim)
+    self.lay2 = nn.Linear(self.mlp_intermediate_dim,self.outdim)
+    self.activ = activ() # test out different activations
     self.dropout = nn.Dropout(HIDDEN_DROPOUT_PROB)
+
   def forward(self,x):
-    return self.dropout(self.lay2(self.activ(self.lay1(x))))
+    return self.dropout(self.lay2(self.activ(self.lay1(x)))) # missing Ocaml pipelining rn :(
