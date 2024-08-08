Fixed Loss function multiplication broadcasting
- Added to heatmap-> joint function

models/loss.py
@@ -14,7 +14,7 @@ class JointsMSELoss(nn.Module):
     self.mse = nn.MSELoss(reduction="mean")
     self.use_visibility_weight = use_visibility_weight
     
-  def forward(self, output, target, target_weight):
+  def forward(self, output, target, target_weights):
     """
     Params: 
       - output: model output (batch_size, num_joints, height?, width)
@@ -25,17 +25,18 @@ class JointsMSELoss(nn.Module):
 
     batch_size = output.shape[0]
     num_joints = output.shape[1] # get #joints for this image of (batch_size, num_joints, height, width)
-    heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1) # reshape heatmap predictions
-    heatmaps_target = target.reshape((batch_size, num_joints, -1)).split(1, 1) # reshape target values
 
     loss = 0
 
     for joint_idx in range(num_joints):
-      heatmap_pred = heatmaps_pred[joint_idx].squeeze(1)
-      heatmap_target = heatmaps_target[joint_idx].squeeze(1)
+      
+      heatmap_pred = output[:,joint_idx,:,:]
+      heatmap_target = target[:,joint_idx,:,:]
+
+      target_weight = target_weights[:,joint_idx].unsqueeze(1).unsqueeze(1) # (batch_size, 1, 1)
 
       if self.use_visibility_weight:
-        loss +=self.mse(target_weight[:,joint_idx] * heatmap_pred, target_weight[:,joint_idx]* heatmap_target)
+        loss +=self.mse(target_weight * heatmap_pred, target_weight* heatmap_target) # multiplication will broadcast 
       else:
         loss +=self.mse(heatmap_pred, heatmap_target)
     # average loss per joint because number of joints in images are variable